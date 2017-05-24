8f8d25875d53c55c695b07927b9148abd5d9a9b7
sevenling <sevenlove_ly@163.com>
2017-05-24 13:40:23 +0800

    999财经直播APP上线宣传

---
+++ APPonline/js/appload.js
@@ -0,0 +1,41 @@
+﻿$(document).ready(function(){	
+	// 获取终端的相关信息
+    var Terminal = {
+        // 辨别移动终端类型
+        platform : function(){
+            var u = navigator.userAgent, app = navigator.appVersion;
+            return {
+                // android终端或者uc浏览器
+                android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1,
+                // 是否为iPhone或者QQHD浏览器
+                iPhone: u.indexOf('iPhone') > -1 ,
+                // 是否iPad
+                iPad: u.indexOf('iPad') > -1,
+                //是否微信
+                micromessenger: !!(~navigator.userAgent.toLowerCase().indexOf("micromessenger"))
+            };
+        }(),
+        // 辨别移动终端的语言：zh-cn、en-us、ko-kr、ja-jp...
+            language : (navigator.browserLanguage || navigator.language).toLowerCase()
+        }
+ 
+        if(Terminal.platform.micromessenger){
+            //微信端
+            $('#appDownload').attr('href','loadApp.html');     
+    }else{
+        // 根据不同的终端，跳转到不同的地址
+        var theUrl = 'http://m.99live.com';
+        var ios_download_site = 'https://itunes.apple.com/us/app/99jiu/id1132509294?l=zh&ls=1&mt=8';
+        var android_download_site = 'http://www.99live.com/app/999_1.1.3.apk';
+
+        if(Terminal.platform.android){
+            theUrl = android_download_site
+        }else if(Terminal.platform.iPhone){
+            theUrl = ios_download_site;
+        }else if(Terminal.platform.iPad){
+            theUrl = ios_download_site
+        }
+ 
+        $('#appDownload').attr('href',theUrl);           
+    }
+})
---
+++ APPonline/js/global.js
@@ -0,0 +1,96 @@
+//移动端页面适配
+(function (doc, win) {
+  var docEl = doc.documentElement,
+    resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+    recalc = function () {
+      var clientWidth = docEl.clientWidth;
+      if (!clientWidth) return;
+      docEl.style.fontSize = 100 * (clientWidth / 640) + 'px';
+    };
+
+  if (!doc.addEventListener) return;
+  win.addEventListener(resizeEvt, recalc, false);
+  doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
+
+$(document).ready(function(){
+	function audioAutoPlay(id){
+		var audio = document.getElementById(id),
+		play = function(){
+			audio.play();
+			document.removeEventListener("touchstart",play, false);
+		};
+		audio.play();
+		document.addEventListener("WeixinJSBridgeReady", function () {//微信
+			play();
+		}, false);
+		document.addEventListener('YixinJSBridgeReady', function() {//易信
+			play();
+		}, false);
+		document.addEventListener("touchstart",play, false);
+	}
+  audioAutoPlay('MusicBox');
+  //打开页面自动播放背景音乐
+	
+	var MusicBox = document.getElementById("MusicBox");
+	var bol=true;
+	$("#audio_btn img").on("touchstart",function(){
+		if(bol){
+			$(this).removeClass('rotate');
+			MusicBox.pause();
+			bol=false;
+		}
+		else{
+			$(this).addClass('rotate');
+			MusicBox.play();			
+			bol=true;
+		}	
+		return bol;
+	});   //音乐按键的操作
+	
+  var swiper = new Swiper('.swiper-container', {   //引入swiper
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    direction: 'vertical',
+    onSlideChangeEnd: function(swiper){
+    	if(!$('.part6').hasClass("swiper-slide-active")){
+	    	$('#ptjs').removeClass('active');
+	    }
+    	switch (swiper.activeIndex){
+    		case 0: 
+			setState($(".part1"));
+    		break;
+    		
+    		case 1: 
+			setState($(".part2"));
+    		break;
+    		
+     		case 2: 
+			setState($(".part3"));
+    		break;
+    		
+      		case 3: 
+			setState($(".part4"));
+    		break;
+    		
+     		case 4: 
+			setState($(".part5"));
+    		break; 	
+    	}    	
+    }    
+  });
+
+	setState($(".part1"));   //设置第一页可见
+	
+	function setState(option){
+		option.find("p").css("display","block");
+		option.find("div").css("display","block");
+		option.siblings("div").find("p").css("display","none");
+		//option.siblings("div").find("div").css("display","none");
+	}   //上下滑动来触发页面的切换
+	
+	$('#more').click(function(){
+		$('#ptjs').addClass('active');
+		$('#ptjs').css("display","flex");
+	})   //点击更多跳转到平台介绍界面
+})
